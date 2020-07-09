a4c4573a3604f5ba339c1ad8aabe2571df9b1bdf
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 13:19:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,7 +26,6 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
-  #new_value=aoh_update[row][key]
- new_value=aoh_update[row][key]
- aoh_update<< a
+  new_value=aoh[row][key]
+ 
 end
